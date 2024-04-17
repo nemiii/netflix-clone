26b1ad09057829968c92db6498788637c0755f75
Nemi Amin <nemiamin@outlook.com>
2024-04-17 15:30:51 +0530

    Completed UI of Profile and 404 page

--- src/pages/no-page/NoPage.js
+++ src/pages/no-page/NoPage.js
@@ -1,5 +1,18 @@
 const NoPage = () => {
-    return <h1>404</h1>;
-  };
-  
-  export default NoPage;
+  return (
+    <div className="page-single-2">
+      <div className="container">
+        <div className="row">
+          <div className="middle-content">
+            <a href="index.html"><img className="md-logo" src="images/logo1.png" alt="" /></a>
+            <img src="images/uploads/err-img.png" alt="" />
+            <h1>Page not found</h1>
+            <a href="/" className="redbtn">go home</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NoPage;
--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ Component }) => {
- const [isAuthenticated, setIsAuthenticated] = useState(false);
+ const [isAuthenticated, setIsAuthenticated] = useState(true);
 
  // Your authentication logic goes here...
   return isAuthenticated ? <Component /> : <Navigate to="/" />;
